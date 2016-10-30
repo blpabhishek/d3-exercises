39517238ce96274232de17a64d394a7b2ac8e491
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-10-30 11:58:48 +0530

    Extracted the data in the JSON file.

--- students.js
+++ students.js
@@ -1,21 +1,12 @@
-var students = [
-	{name:'ramesh',subject:'maths',score:87},
-	{name:'suresh',subject:'maths',score:45},
-	{name:'pokemon',subject:'english',score:65},
-	{name:'mary',subject:'kannada',score:44},
-	{name:'riya',subject:'science',score:72},
-	{name:'katie',subject:'social studies',score:82},
-	{name:'katie',subject:'maths',score:98},
-	{name:'ramesh',subject:'bengali',score:25},
-	{name:'suresh',subject:'science',score:55},
-	{name:'riya',subject:'tamil',score:75},
-	{name:'pokemon',subject:'sports',score:95},
-	{name:'pokemon',subject:'social studies',score:32}
-];
-
-var getSubjects = function(students){
-	return students.map((d)=>d.subject).filter((v,i,a)=>a.indexOf(v) == i);
+var getUniqSubjects = function(students){
+	return students.map((d)=>d.subject).filter((s,i,a)=> a.indexOf(s) == i);
 }
+
+var sortBy = function(key){
+	return d3.selectAll('.students').sort(function(a,b){ 
+		return d3.ascending(a[key],b[key]); 
+	});
+};
 
 var displayStudents = function (data,subjects) {
 	var colorScale = d3.scaleOrdinal()
@@ -42,8 +33,7 @@ var displayStudents = function (data,subjects) {
 	sortButton	
 		.enter()
 		.append('button')
-		.attr("id",function(d){return d;})
-		.on("click",function(r){ return sortBy(r.toLowerCase());})
+		.on("click",function(k){ return sortBy(k.toLowerCase());})
 		.text(function(d){return d;});
 
 	//Legend
@@ -58,11 +48,9 @@ var displayStudents = function (data,subjects) {
 		.text(function(d){return d;});
 }
 
-var sortBy = function(key){
-	return d3.selectAll('.students').sort(function(a,b){ 
-		return d3.ascending(a[key],b[key]); 
-	});
-};
+var parse = function(err,students){
+	var subjects = getUniqSubjects(students);
+	displayStudents(students,subjects);
+}
 
-var subjects = getSubjects(students);
-displayStudents(students,subjects);
+d3.json("./students.json",parse)
