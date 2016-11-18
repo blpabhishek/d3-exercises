9027de6c649153e482631b8897c73d4fc4ab4751
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-11-18 14:21:50 +0530

    Used curves to give smothing to lines.

--- js/axisLine.js
+++ js/axisLine.js
@@ -4,13 +4,17 @@ var margin = 30;
 var innerWidth = width - (2 * margin);
 var innerHeight = height - (2 * margin);
 
+var linePoints = [{x:0,y:5},{x:1,y:9},{x:2,y:7},{x:3,y:5},{x:4,y:3},{x:6,y:4},{x:7,y:2},{x:8,y:3},{x:9,y:2}];
+	var range = 10;
+	var data = d3.range(range);
+
 var translate = function(x, y) {
     return 'translate(' + x + ',' + y + ')';
 }
-var draw = function() {
-	var linePoints = [{x:0,y:5},{x:1,y:9},{x:2,y:7},{x:3,y:5},{x:4,y:3},{x:6,y:4},{x:7,y:2},{x:8,y:3},{x:9,y:2}];
-	var data = d3.range(10);
+
+var draw = function(curve) {
 	var container = d3.select('.container');
+	container.append('h3').text(curve);
 
 	var svg = container.append('svg')
 		.attr('width',width)
@@ -38,12 +42,14 @@ var draw = function() {
         .attr('transform', translate(margin, margin));
 
 	var line = d3.line()
-    	.x(function(d) { return XScale(d.x/10);})
-    	.y(function(d) { return YScale(d.y/10);});
+    	.x(function(d) { return XScale(d.x/range);})
+    	.y(function(d) { return YScale(d.y/range);})
+    	.curve(d3[curve]);
 
     var sinX = d3.line()
-    	.x(function(d){return XScale(d/10);})
-    	.y(function(d){return YScale((Math.sin(d)/10)+0.5);});
+    	.x(function(d){return XScale(d/range);})
+    	.y(function(d){return YScale((Math.sin(d)/range)+0.5);})
+    	.curve(d3[curve]);
 
     var lineGroup =svg.append('g')
         .classed('lineGroup', true)
@@ -64,14 +70,19 @@ var draw = function() {
 	var circle = lineGroup.selectAll('.line circle').data(linePoints);
 	circle.enter()
 		.append('circle')
-		.attr('cx',function(d) { return XScale(d.x/10);})
-    	.attr('cy',function(d) { return YScale(d.y/10);});
+		.attr('cx',function(d) { return XScale(d.x/range);})
+    	.attr('cy',function(d) { return YScale(d.y/range);});
 
     circle = lineGroup.selectAll('.sinX circle').data(data);
 	circle.enter()
 		.append('circle')
-		.attr('cx',function(d) { return XScale(d/10);})
-    	.attr('cy',function(d) { return YScale((Math.sin(d)/10)+0.5);});
+		.attr('cx',function(d) { return XScale(d/range);})
+    	.attr('cy',function(d) { return YScale((Math.sin(d)/range)+0.5);});
 }
 
-window.onload = draw;
+var main = function(){
+	var curves = Object.keys(d3).filter((e)=>e.match('curve'));
+	curves.forEach(draw);
+}
+
+window.onload = main;
