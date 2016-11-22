643100938be8bc70d761c0ce2ca92854c9c34d16
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-11-22 22:37:36 +0530

    Implemented all other styles in the piechart examples.

--- js/pieChart.js
+++ js/pieChart.js
@@ -1,31 +1,48 @@
-var width = 700;
-var height = 800;
+var width = 600;
+var height = 500;
 var data = [1, 1, 2, 2, 1, 2, 1] ;
-var draw = function() {
+
+
+var draw = function(option) {
 	var container = d3.select('.container');
 
 	var g = container.append('svg')
 		.attr('width',width)
 		.attr('height',height)
 		.append('g')
-		.attr('transform','translate(300,300)');
+		.attr('transform','translate(300,150)');
 
 	var colorScale = d3.scaleOrdinal()
 		.domain([0,10])
 		.range(d3.schemeCategory20);
 
 	var arc = d3.arc()             
-        .outerRadius(200)
-        .innerRadius(0);
+        .outerRadius(option.outerRadius)
+        .innerRadius(option.innerRadius);
 
 	var pie = d3.pie()
 	.value(function(d){return d;})
+	.startAngle(option.startAngle)
+	.endAngle(option.endAngle);
 
 	g.selectAll('path').data(pie(data))
 		.enter()
 		.append('path')
 		.attr('d',arc)
-		.attr('fill',function(d,i){return colorScale(i);})			
+		.attr('fill',function(d,i){return colorScale(i);});
 }
 
-window.onload = draw;
+var toRadian = function(d){
+	return d*(Math.PI/180);
+}
+
+var loadStyles = function(){
+	var range = [{innerRadius:0,outerRadius:150,startAngle:toRadian(0),endAngle:toRadian(360)},
+				{innerRadius:75,outerRadius:150,startAngle:toRadian(180),endAngle:toRadian(360)},	
+				{innerRadius:0,outerRadius:150,startAngle:toRadian(180),endAngle:toRadian(360)},
+				{innerRadius:75,outerRadius:150,startAngle:toRadian(0),endAngle:toRadian(360)}];
+
+	range.forEach(draw);
+}
+
+window.onload = loadStyles;
