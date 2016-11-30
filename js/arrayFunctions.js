60ac24f9c389c2acecff3f7f8213a0c581a6a802
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-11-30 18:33:31 +0530

    Added demonstration of arrayFunctions.

---
+++ js/arrayFunctions.js
@@ -0,0 +1,116 @@
+var width = 700;
+var height = 800;
+var margin = 30;
+var innerWidth = width - (2 * margin);
+var innerHeight = height - (2 * margin);
+
+var data = [9,10,4,2,5,6,4,8,7,3];
+var XScale,YScale,colorScale;
+
+var translate = function(x, y) {
+    return 'translate(' + x + ',' + y + ')';
+}
+
+var createSvg = function(){
+	var container = d3.select('.container');
+	var svg = container.append('svg')
+		.attr('width',width)
+		.attr('height',height);
+	return svg;
+}
+
+var drawChart = function() {
+	var svg = createSvg();
+	var range = d3.extent(data);
+
+	XScale = d3.scaleLinear()
+		.domain([0,data.length])
+		.range([0,innerWidth])
+
+	YScale = d3.scaleLinear()
+		.domain([0,d3.sum(data)])
+		.range([innerHeight,0]);
+
+	var xAxis = d3.axisBottom(XScale);
+    var yAxis = d3.axisLeft(YScale);
+
+    svg.append('g')
+        .call(xAxis)
+        .classed('xAxis', true)
+        .attr('transform', translate(margin, innerHeight+margin));
+
+    svg.append('g')
+        .call(yAxis)
+        .classed('yAxis', true)
+        .attr('transform', translate(margin, margin));
+
+    var dataGroup = svg.append('g')
+        .classed('data', true)
+        .attr('transform', translate(margin, margin)) 
+
+    var barWidth = innerWidth / (data.length * 2);
+
+    var alignBarSpace = barWidth/2;
+
+    var bars = dataGroup.selectAll('rect').data(data);
+
+	bars.enter().append("rect")
+      .attr("x", function(d,index) { return XScale(index+1)-alignBarSpace;})
+      .attr("y", function(d) { return YScale(d); })
+      .attr("height", function(d) { return innerHeight - YScale(d); })
+      .attr("width", barWidth)
+
+   addButtons();
+}
+
+var click = function(value,group){
+	var response;
+	if(value=='quantile')
+		response = prompt('Enter the quantile range 0-1','0.5');
+	
+	var lineData = d3[value](data,response);
+	var linePoint = lineData.map ? lineData.map((d)=>YScale(d)):[YScale(lineData)];
+
+	var line = group.selectAll('line').data(linePoint);
+
+	line.enter().append('line')
+		.attr('x1',0)
+		.attr('y1',function(d){return d;})
+		.attr('x2',width)
+		.attr('y2',function(d){return d;})
+		.classed('indicator',true)
+		.attr('stroke',colorScale(value))
+
+	line
+		.attr('y1',function(d){return d;})
+		.attr('y2',function(d){return d;})
+		.attr('stroke',colorScale(value))
+
+	line.exit().remove();
+}
+
+var addButtons = function(){
+	var functions = ['min', 'max', 'extent', 'sum', 'mean', 'median', 'quantile', 'variance', 'deviation'];
+	colorScale = d3.scaleOrdinal()
+		.domain(functions)
+		.range(d3.schemeCategory10);
+
+	var container = d3.select('.container');
+
+	var div = container.append('div').text('Functions:');
+
+	var buttons = div.selectAll('button').data(functions);
+
+	var group = d3.selectAll('.data');
+
+	buttons	
+		.enter()
+		.append('button')
+		.style('background-color',function(d){return colorScale(d);})
+		.text(function(d){return d;})
+		.on('click',function(value){ 
+			click(value,group);
+		});
+}
+
+window.onload = drawChart;
